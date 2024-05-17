f01d488700a3e3c8f8198e297924901877bcf100
B3LM0N <mbelmontesan@uoc.edu>
2024-05-17 16:19:44 +0200

    Añadiendo Interfaces
    Ya quedan menos

--- src/main/java/Vista/FX/Controlador/ControladorFX.java
+++ src/main/java/Vista/FX/Controlador/ControladorFX.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -19,6 +18,7 @@ public class ControladorFX {
         // Cerrar el Stage
         stage.close();
     }
+
     @FXML
     protected void menuGestionExcursiones() throws IOException {
         // Carga el archivo FXML
@@ -29,16 +29,18 @@ public class ControladorFX {
         stage.setScene(new Scene(loader.load()));
         stage.show();
     }
+
     @FXML
-    protected void crearExcursion() throws IOException {
+    protected void crearExcursionForm() throws IOException {
         // Carga el archivo FXML
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Excursiones/crearExcursion.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Excursiones/crearExcursionForm.fxml"));
         Stage stage = new Stage();
         stage.setTitle("Crear Excursion");
         stage.initModality(Modality.WINDOW_MODAL);
         stage.setScene(new Scene(loader.load()));
         stage.show();
     }
+
     @FXML
     protected void mostrarExcursionesPorFechas() throws IOException {
         // Carga el archivo FXML
@@ -49,6 +51,7 @@ public class ControladorFX {
         stage.setScene(new Scene(loader.load()));
         stage.show();
     }
+
     @FXML
     protected void menuGestionSocios() throws IOException {
         // Carga el archivo FXML
@@ -61,11 +64,180 @@ public class ControladorFX {
     }
 
     @FXML
+    protected void crearSocioForm() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/crearSocioTipo.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Crear Socio");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    /*
+    @FXML
+    protected void cambiarSeguroSocio() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/cambioSeguro.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Cambio de Seguro");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+    @FXML
+    protected void eliminarSocio() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/eliminarSocio.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Eliminar un Socio");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+     */
+    @FXML
+    protected void mostrarSociosSelec() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/mostrarSociosSelec.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Mostrar Socios");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarTodosSocios() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/mostrarSocios.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Mostrar Socios");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarSocioEstandar() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/mostrarSocios.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Mostrar Socios");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarSocioFederado() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/mostrarSocios.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Mostrar Socios");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarSocioInfantil() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Socios/mostrarSocios.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Mostrar Socios");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    /*@FXML
+    protected void mostrarFacturaSelec(){}
+        @FXML
+    protected void mostrarFactura(){
+    }
+     */
+    @FXML
     protected void menuGestionInscripciones() throws IOException {
         // Carga el archivo FXML
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Menus/menuGestionInscripciones.fxml"));
         Stage stage = new Stage();
         stage.setTitle("Menu Excursiones");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void crearInscripcion() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/crearInscripcionForm.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Crear Inscripcion");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void eliminarInscripcion() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/eliminarInscripcion.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Eliminar Inscripcion");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+    @FXML
+    protected void mostrarInscripcionSelec() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/mostrarInscripcionesSelec.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Lista de Inscripciones");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarInscripcionSinFiltro() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/mostrarInscripciones.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Lista de Inscripciones sin Filtrar");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarInscripcionPorSocio() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/mostrarInscripciones.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Lista de Inscripciones por Socio");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarInscripcionPorFecha() throws IOException {
+        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/mostrarInscripciones.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Lista de Inscripciones por Fecha");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    protected void mostrarInscripcionPorSocioYFecha() throws IOException {        // Carga el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/Inscripciones/mostrarInscripciones.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Lista de Inscripciones por Socio y Fecha");
         stage.initModality(Modality.WINDOW_MODAL);
         stage.setScene(new Scene(loader.load()));
         stage.show();
